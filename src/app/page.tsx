94ab24710e58331e3c99ed3e7547801c70cdb9db
erickpeixoto <erickepeixoto@gmail.com>
2024-04-25 08:13:20 -0300

    chore: get data test from the mock api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 export default async function Home() {
-  const data = await fetch("https://662a34a567df268010a2f99a.mockapi.io/api/v1/users");
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`);
   const users = await data.json();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      here
-      
+      {JSON.stringify(users, null, 2)}
     </main>
   );
 }
