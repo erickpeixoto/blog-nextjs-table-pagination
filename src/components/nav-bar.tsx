b6404c9dd86cf608df141ff49c8c5b4b3f7bab4c
erickpeixoto <erickepeixoto@gmail.com>
2024-04-27 15:53:22 -0300

    feat: add new Add button

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,13 +1,17 @@
 import { Search } from "@/components/search";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export async function NavBar() {
   return (
     <div className="flex justify-between p-5 border mb-5">
       <div className="strong text-slate-800 font-semibold text-2xl text-center       ">
-        <Link href="/" prefetch={false}>Demo App List</Link>
-     </div>
-      <div>
+        <Link href="/">App List</Link>
+      </div>
+      <div className="flex gap-x-2">
+        <Link href="/user/new">
+          <Button variant={"outline"}>New</Button>
+        </Link>
         <Search />
       </div>
     </div>
