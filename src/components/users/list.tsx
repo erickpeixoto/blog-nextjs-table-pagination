ded8bf192e3a80c9af3d4535b6f6665c33cb0e2a
erickpeixoto <erickepeixoto@gmail.com>
2024-04-29 09:28:39 -0300

    feat: call delete action on the list page

--- src/components/users/list.tsx
+++ src/components/users/list.tsx
@@ -1,21 +1,38 @@
 "use client";
 
 import { DataTable } from "@/components/data-table";
-import { columns } from "@/components/users/columns";
-import { useQuery } from "@tanstack/react-query";
-import { getUsers } from "@/actions/user";
+import { Columns } from "@/components/users/columns";
 import { useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getUsers, deleteUser } from "@/actions/user";
+import { User } from "@/lib/schemas/user";
 
 export function List() {
   const params = useSearchParams();
   const search = params.get("search");
 
-  const { data } = useQuery({
+  const { data: users } = useQuery({
     queryKey: ["users"],
     queryFn: () => getUsers(search as string),
-    staleTime: 1000,
-    enabled: !search,
   });
 
-  return <DataTable columns={columns} data={data} />;
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationKey: ["users"],
+    mutationFn: (id: string) => deleteUser(id),
+    onSuccess(data) {
+      toast.success("User deleted successfully");
+      queryClient.setQueryData<Partial<User>[]>(["users"], (currentUsers) => {
+        return currentUsers?.filter((user) => user.id !== data.id);
+      });
+    },
+  });
+  const handleDelete = (id: string | undefined) => {
+    mutation.mutate(id!);
+  };
+  const columns = Columns({ handleDelete });
+
+  return <DataTable columns={columns} data={users} />;
 }
